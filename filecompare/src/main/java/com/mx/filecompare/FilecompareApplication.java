19b446fa584846facee08faf2a8e351def146759
maxu78 <805955913@qq.com>
2018-01-23 09:38:05 +0800

    Initial commit

---
+++ filecompare/src/main/java/com/mx/filecompare/FilecompareApplication.java
@@ -0,0 +1,12 @@
+package com.mx.filecompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilecompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilecompareApplication.class, args);
+	}
+}
